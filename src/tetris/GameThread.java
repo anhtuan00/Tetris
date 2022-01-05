1cefafb10e25cdf55efb79ac96b68b2c06518cde
Pham Dang Khoa <59013970+Khoa100500@users.noreply.github.com>
2022-01-05 22:56:56 +0700

    Implementing runable instead of extending thread
    When we extend Thread class, we can’t extend any other class even we require and When we implement Runnable, we can save a space for our class to extend any other class in future or now. When we extend Thread class, each of our thread creates unique object and associate with it. When we implements Runnable, it shares the same object to multiple threads.

--- src/tetris/GameThread.java
+++ src/tetris/GameThread.java
@@ -12,59 +12,62 @@ import java.util.logging.Logger;
  *
  * @author Admin
  */
-public class GameThread extends Thread {
-
+public class GameThread implements Runnable {
     private GameArea ga;
     private GameForm gf;
     private int score;  
     private int level = 1;
     private int scorePerLevel = 3;
-    
+    private Thread thread;
     private int pause = 1000;
     private int speedupByLevel = 100;
-
+    private boolean isRunning;
+    
     public GameThread(GameArea ga, GameForm gf) {
         this.ga = ga;
         this.gf = gf;
+
+    }
+    
+    public synchronized void start() {
+    	isRunning = true;
+    	thread = new Thread(this);
+    	thread.start();
+    } 
+    public synchronized void stop() {
+    	isRunning = false;
+    	try {
+    		thread.join();
+    	} catch(InterruptedException e) {
+    		e.printStackTrace();
+    	}
     }
 
     public void run() {
-
-        while (true) {
-
+        while (isRunning) {
             ga.spawnBlock();
-
             while (ga.moveBlockDown() == true) {
                 try {
-
                     Thread.sleep(pause);
                 } catch (InterruptedException ex) {
                     Logger.getLogger(GameThread.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
-            
             if (ga.isBlockOutOfBounds()) {
                 
                 System.out.println("Game Over");
-                break;
-                
-            }
-            
+                this.stop();
+                break;  
+            } 
             ga.moveBlockToBackground();
             score = score + ga.clearLines();
             gf.updateScore(score);
-            
             int lvl = score / scorePerLevel + 1;
             if (lvl > level) {
-                
                 level = lvl;
                 gf.updateLevel(level);
-                pause -= speedupByLevel;
-                
+                pause -= speedupByLevel;   
             }
-
         }
-
     }
-
 }
