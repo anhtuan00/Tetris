c4210bbbfc39d0f965452911da32d1185eab0349
anhtuan00 <67965462+anhtuan00@users.noreply.github.com>
2022-01-07 20:19:00 +0700

    fix bug

--- src/tetris/TetrisBlock.java
+++ src/tetris/TetrisBlock.java
@@ -94,18 +94,6 @@ public class TetrisBlock {
     
     
 
-    /**
-     * @param x the x to set
-     */
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    /**
-     * @param y the y to set
-     */
-    public void setY(int y) {
-        this.y = y;
-    }
+    
     
 }
